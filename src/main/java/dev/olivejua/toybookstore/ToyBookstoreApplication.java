f4f8e8bbff3a4dbeb8fe2144c9c787d9337829cb
olivejua <tmfrl4710@gmail.com>
2021-04-01 22:20:45 +0900

    Initial commit

---
+++ src/main/java/dev/olivejua/toybookstore/ToyBookstoreApplication.java
@@ -0,0 +1,13 @@
+package dev.olivejua.toybookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/olivejua/toybookstore/ToyBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.olivejua.toybookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
